a785c9d0a8216c2e61899134d8d1835900646158
unknown <fredericreact@gmail.com>
2023-01-26 13:32:28 +0800

    Working with Function Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
@@ -8,8 +9,14 @@ function App() {
     new Todo('Learn Typescript')
   ]
 
+
+  const addTodoHandler = (todoText: string) => {
+
+  }
+
   return (
     <div >
+     <NewTodo onAddTodo={addTodoHandler}/>
      <Todos items={todos}/>
     </div>
   );
